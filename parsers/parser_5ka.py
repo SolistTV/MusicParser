960a539f6504f0a0eb8d9e060c0ca95e1a99a48b
home_n_belov <n.belov@cbonds.info>
2025-05-08 00:15:24 +0300

    #Тестовое задание парсер 5ки (final)

--- parsers/parser_5ka.py
+++ parsers/parser_5ka.py
@@ -1,11 +1,8 @@
 import json
+import time
 import urllib.parse
 import csv
 import logging
-import asyncio
-from typing import Tuple
-
-from src import config_5ka
 
 from selenium import webdriver
 from selenium_stealth import stealth
@@ -13,56 +10,64 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.options import Options
+from concurrent.futures import ThreadPoolExecutor
+
+from src import config_5ka
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+"""
+Парсер продуктов 5ки по ключевой фразе
+"""
+
 
 class Parser5ka:
-    def __init__(self):
-        self.base_url = config_5ka.BASE_URL
-
-    async def run(self, search_phrase: str) -> None:
+    def run(self) -> None:
         logging.info('== Старт сбора данных ==')
         with open(config_5ka.RESULTS_FILE, "w", newline="", encoding="UTF-8") as file:
             file.truncate(0)
             writer = csv.writer(file)
             writer.writerow(["Артикул", "Наименование", "Цена"])
 
-        # loop = asyncio.get_running_loop()
         offset = 0
         processing = True
-        tasks = []
-        print('набираем таски')
+        urls = []
+
+        drivers = [self.__browser_init() for _ in range(config_5ka.REQUESTS_LIMIT)]
         while processing:
-            print(len(tasks))
-            tasks.append(asyncio.create_task(self.__get_data_from_source_by_phrase(search_phrase, offset)))
+            urls.append(self.__create_link(config_5ka.PHRASE, offset))
+            if len(urls) == config_5ka.REQUESTS_LIMIT:
+                processing = self.__urls_processing(urls, drivers)
+                urls = []
+                logging.info('Принудительная задержка для избегания бана')
+                time.sleep(3)
             offset += config_5ka.LIMIT
-            if len(tasks) == config_5ka.REQUESTS_LIMIT:
-                print('Обработка тасок')
-                tasks, processing = await self.__tasks_processing(tasks)
         else:
-            if len(tasks) > 0:
-                await self.__tasks_processing(tasks)
+            if len(urls) > 0:
+                self.__urls_processing(urls, drivers)
             logging.info('данные собраны')
+
+        for driver in drivers:
+            driver.close()
+            driver.quit()
 
         logging.info('== Завершение сбора данных ==')
 
     @staticmethod
-    async def __browser_init() -> webdriver.Chrome:
-        """
-        TODO разнести использование браузера на отдельные процессы.
-         Параллельную работу нормально настроить нормально пока не вышло...
-        :return:
-        """
+    def __create_link(phrase: str, offset: int) -> str:
+        encode_phrase = urllib.parse.quote(phrase)
+        return f"{config_5ka.SEARCH_URL}&q={encode_phrase}&limit={config_5ka.LIMIT}&offset={offset}"
+
+    @staticmethod
+    def __browser_init() -> webdriver.Chrome:
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920x1080")
         options.add_argument('"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"')
         options.add_argument("--disable-blink-features=AutomationControlled")
-        # options.add_argument(f"user-data-dir={main_config.USER_DIR}")
-        loop = asyncio.get_running_loop()
-        driver = await loop.run_in_executor(None, webdriver.Chrome, options)
+        driver = webdriver.Chrome(options=options)
         stealth(driver,
                 languages=["en-US", "en"],
                 vendor="Google Inc.",
@@ -72,45 +77,6 @@ class Parser5ka:
                 fix_hairline=True)
 
         return driver
-
-    async def __get_data_from_source_by_phrase(self, phrase: str, offset: int) -> list:
-        print(f'Инит браузера {offset}')
-        driver = await self.__browser_init()
-        print(f'Инит браузера завершен {offset}')
-        results = []
-        encode_phrase = urllib.parse.quote(phrase)
-        url = f"{config_5ka.SEARCH_URL}&q={encode_phrase}&limit={config_5ka.LIMIT}&offset={offset}"
-        logging.info(f"Обработка страницы: {url}")
-        driver.get(url)
-        try:
-            element = WebDriverWait(driver, 10).until(
-                ec.presence_of_element_located((By.XPATH, '//pre'))
-            )
-
-            content = element.text
-        except Exception as ex:
-            logging.error('Не удалось найти блок с данными, отвалился браузер')
-            logging.error(ex)
-            return results
-        finally:
-            driver.close()
-            driver.quit()
-
-        if len(content) == 0:
-            logging.error('Не удалось найти блок с данными, изменился формат ответа')
-            return results
-
-        logging.info(f"Страница обработана: {url}")
-        return self.__collect_data(str(content))
-
-    def __collect_data(self, content: str) -> list:
-        results = []
-        data = json.loads(content)
-        if len(data) == 0:
-            logging.error('Не удалось получить данные из json')
-            return results
-        results = [result for item in data['products'] if (result := self.__collect_result(item))]
-        return results
 
     @staticmethod
     def __collect_result(item: dict):
@@ -132,29 +98,60 @@ class Parser5ka:
             price,
         ]
 
-    async def __tasks_processing(self, tasks) -> Tuple[list, bool]:
-        processing = True
-        try:
-            current_data = await asyncio.gather(*tasks)
-            with open(config_5ka.RESULTS_FILE, "a", encoding="UTF-8", newline='') as file:
-                writer = csv.writer(file)
-                print('запись в файл')
-                [self.__save_item(writer, item) for result_items in current_data for item in result_items if item]
-
-            if not current_data[-1] or len(current_data[-1]) != config_5ka.LIMIT:
-                processing = False
-        except asyncio.exceptions.InvalidStateError:
-            processing = False
-        finally:
-            tasks = []
-
-        return tasks, processing
-
     @staticmethod
     def __save_item(writer, item) -> None:
         writer.writerow(item)
 
+    def __urls_processing(self, urls: list, drivers: list[webdriver.Chrome]) -> bool:
+        processing = True
+        counter_urls = len(urls)
+        with ThreadPoolExecutor(max_workers=counter_urls) as executor:
+            futures = [
+                executor.submit(self.__get_data_from_source_by_phrase, url, driver)
+                for url, driver in zip(urls, drivers)
+            ]
+
+            with open(config_5ka.RESULTS_FILE, "a", encoding="UTF-8", newline='') as file:
+                writer = csv.writer(file)
+                for future in futures:
+                    if len(future.result()) < config_5ka.LIMIT:
+                        processing = False
+                    [self.__save_item(writer, item) for item in future.result() if item]
+
+        return processing
+
+    def __get_data_from_source_by_phrase(self, url: str, driver: webdriver.Chrome) -> list:
+        results = []
+        logging.info(f"Обработка страницы: {url}")
+        driver.get(url)
+        try:
+            element = WebDriverWait(driver, 10).until(
+                ec.presence_of_element_located((By.XPATH, '//pre'))
+            )
+
+            content = element.text
+        except Exception as ex:
+            logging.error('Не удалось найти блок с данными, отвалился браузер')
+            logging.error(ex)
+            return results
+
+        if len(content) == 0:
+            logging.error('Не удалось найти блок с данными, изменился формат ответа')
+            return results
+
+        logging.info(f"Страница обработана: {url}")
+        return self.__collect_data(str(content))
+
+    def __collect_data(self, content: str) -> list:
+        results = []
+        data = json.loads(content)
+        if len(data) == 0:
+            logging.error('Не удалось получить данные из json')
+            return results
+        results = [result for item in data['products'] if (result := self.__collect_result(item))]
+        return results
+
 
 if __name__ == '__main__':
     parser = Parser5ka()
-    asyncio.run(parser.run(config_5ka.PHRASE))
+    parser.run()
